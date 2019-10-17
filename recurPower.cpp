27f383cdf7077ae6a330d1415d671588416a0eba
Darragh Moloney <56196541+dmolo87@users.noreply.github.com>
2019-10-17 21:17:16 +0100

    Add files via upload

---
+++ recurPower.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+double recurPower(int num, int power) {
+
+    //base case x^0 is 1
+    if(power == 0) {
+        return 1.0;
+    }
+
+    //if power is negative, return
+    //recursive of 1 / x^-1
+    if(power < 0) {
+        int negPower = power * -1;
+        return 1.0 / recurPower(num, negPower);
+    }
+    
+    //if power is odd, return
+    //recursive of x * x^n-1
+    if(power % 2 == 1) {
+        return static_cast<double>(num) * recurPower(num, (power-1));
+    }
+
+    //if power is even, return
+    //x^(n/2) * x^(n/2)
+    if(power % 2 == 0) {
+        //temp var to only calculate once
+        //for efficiency
+        double temp = recurPower(num, (power/2));
+        return temp * temp;
+    } 
+
+}
+
+int main() {
+
+    bool choice = true;
+    do {
+        std::cout << "Enter the number and the exponent.\n";
+        std::cout << ":";
+
+        int num1 = 0;
+        int num2 = 0;
+
+        std::cin >> num1;
+        std::cin >> num2;
+
+        std::cout << num1 << "^" << num2 << " is " << recurPower(num1, num2) 
+            << ".\n";
+
+        std::cout << "Try again? y/n: ";
+        char again;
+        std::cin >> again;
+        
+        if(again == 'N' || again == 'n') {
+            choice = false;
+        }
+
+
+    } while(choice);
+    
+    return 0;
+}
